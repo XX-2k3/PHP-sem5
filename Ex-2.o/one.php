eeb0fdedf0db27886d05c8851ce913d03f94a3ec
XX-2k3 <141943941+XX-2k3@users.noreply.github.com>
2024-07-12 04:17:00 +0000

    completed Ex-2.o

---
+++ Ex-2.o/one.php
@@ -0,0 +1,6 @@
+<!-- Write a PHP program to print 5 to 10 using For. -->
+<?php
+ for($i=10;$i<16;$i++){
+    echo $i."<br>";
+ }
+ ?>
---
+++ Ex-2.o/two.php
@@ -0,0 +1,14 @@
+<!-- Write a PHP program to print 15 to 20 using While and Do While. -->
+<?php
+ $i=15;
+ while($i<21){
+    echo $i."<br>";
+    $i+=1;
+ }
+
+$j = 15;
+do {
+    echo $j . "<br>";
+    $j++;
+} while ($j <= 20);
+?>
